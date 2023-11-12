75e4e3cb5dcc4fb09cbeb4d0d0911feea0d56a2d
Riski Rahmand <79498377+BOFOII@users.noreply.github.com>
2023-11-12 17:27:17 +0700

    REFACTOR: no need DEvirion

--- libasynql/src/poggit/libasynql/base/SqlSlaveThread.php
+++ libasynql/src/poggit/libasynql/base/SqlSlaveThread.php
@@ -50,14 +50,6 @@ abstract class SqlSlaveThread extends Thread implements SqlThread{
 		$this->slaveNumber = self::$nextSlaveNumber++;
 		$this->bufferSend = $bufferSend ?? new QuerySendQueue();
 		$this->bufferRecv = $bufferRecv ?? new QueryRecvQueue();
-
-		if(!libasynql::isPackaged()){
-			/** @noinspection PhpUndefinedMethodInspection */
-			/** @noinspection NullPointerExceptionInspection */
-			/** @var ClassLoader $cl */
-			$cl = Server::getInstance()->getPluginManager()->getPlugin("DEVirion")->getVirionClassLoader();
-			$this->setClassLoaders([Server::getInstance()->getLoader(), $cl]);
-		}
 		$this->start(NativeThread::INHERIT_INI);
 	}
 
